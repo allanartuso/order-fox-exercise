8c06c68c416cade48a367fd991ee21b277f0311f
Allan Artuso <allan.artuso@gmail.com>
2021-10-04 23:37:49 +0200

    feat: 2nd try

--- calculator.ts
+++ calculator.ts
@@ -1,7 +1,8 @@
 enum Operator {
-  SUM=0,
-  MINUS=1,
-  DIVISION=2,
+  SUM = 0,
+  MINUS = 1,
+  DIVISION = 2,
+  MULTIPLICATION = 3,
 }
 
 interface Operation {
@@ -11,27 +12,45 @@ interface Operation {
 
 function calculator(operations: Operation[]) {
   let result = 0;
-const test = operations.sort((a,b)=> b.operator-a.operator)
-    console.log(test);
-    
 
-test.forEach((operation) => {
-    operation.numbers.forEach((number) => {
-      if (operation.operator === Operator.SUM) {
-        result += number;
-      } else if (operation.operator === Operator.DIVISION) {
-        result = result / number;
-      }else if (operation.operator === Operator.MINUS) {
-        result -=  number;
+  operations
+    .sort((a, b) => b.operator - a.operator)
+    .forEach((operation) => {
+        if (operation.operator === Operator.DIVISION) {
+            result += solveDivision(operation.numbers);
+          } else if (operation.operator === Operator.MULTIPLICATION) {
+            result += solveDivision(operation.numbers);
+          } else {
+        operation.numbers.forEach((number) => {
+          if (operation.operator === Operator.SUM) {
+            result += number;
+          } else if (operation.operator === Operator.MINUS) {
+            result -= number;
+          }
+        });
       }
-    });
 
-    console.log(result);
-    
-  });
+      console.log(result);
+    });
 
   return result;
 }
+
+function solveDivision(numbers: number[]): number {
+    let result = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+      result = result / numbers[i];
+    }
+    return result;
+  }
+
+  function solveMultiplication(numbers: number[]): number {
+    let result = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+      result = result * numbers[i];
+    }
+    return result;
+  }
 
 const operations: Operation[] = [
   {
@@ -40,15 +59,12 @@ const operations: Operation[] = [
   },
   {
     operator: Operator.DIVISION,
-    numbers: [4,3],
+    numbers: [4, 3],
   },
   {
     operator: Operator.MINUS,
     numbers: [5],
   },
 ];
-
-
-2 + (4/3 - 5)
 
 console.log(calculator(operations));
